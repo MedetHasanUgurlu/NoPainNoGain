99f6630a9dfc6ed0da13d3801f10d0014351d751
MedetHasanUgurlu <mdt.hsn.76@hotmail.com>
2022-11-16 22:26:55 +0300

    Largest in Array

--- src/LargestGap.java
+++ src/LargestGap.java
@@ -8,7 +8,7 @@ public class LargestGap {
 //		[9, 4, 26, 26, 0, 0, 5, 20, 6, 25, 5]
 //		[0, 0, 4, 5, 5, 6, 9, 20, 25, 26, 26]
 //		... so that we now see that the largest gap in the array is between 9 and 20 which is 11.
-        int[] a = {0, 0, 4, 5, 5, 6, 9, 20, 25, 26, 26};
+        int[] a = {0, 0, 4, 5, 5, 6, 9, 20, 25, 26, 100};
         System.out.println(findLargestGap(a));
 
     }
